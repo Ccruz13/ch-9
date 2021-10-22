6b7e028a8ec3b67f237320414c11f85ebbde071b
Ccruz13 <88520938+Ccruz13@users.noreply.github.com>
2021-10-22 14:37:24 -0400

    added some questions

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
-
+const inquirer = require('inquirer');
+const generatePage = require('./develop/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "what is the title of your project? (Required)",
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('You need to enter a project name!');
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Provide a discription of your project.",
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('You need to enter a project description');
+                return false;
+            }
+        }
+    },
+
+    // Table of content
+    
+    {
+
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
